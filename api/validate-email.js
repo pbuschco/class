89eccb40f0208fa58f48634b38d311dc5f8d4e1d
pbuschco <pbusch@landmarkworldwide.com>
2025-03-13 13:08:22 -0600

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -30,7 +30,7 @@ export default function handler(req, res) {
 
       return res.status(200).json({ success: true, redirectURL });
     } else {
-      return res.status(403).json({ success: false, message: "Email not authorized" });
+      return res.status(403).json({ success: false, message: "Input valid email in Lowercase!" });
     }
   } catch (error) {
     console.error("Error processing the request:", error);
